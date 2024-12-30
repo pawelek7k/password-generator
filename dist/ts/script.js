1db4d0f7222258fb98e644953b6c9532e3439bc0
pawelek <k.pawelek@vp.pl>
2024-12-30 22:32:28 +0100

    Update script.js

--- dist/ts/script.js
+++ dist/ts/script.js
@@ -7,3 +7,21 @@ var options = {
 var passwordListContainer = document.querySelector('.password-list-container');
 var incrementButtons = document.querySelectorAll('.increment');
 var decrementButtons = document.querySelectorAll('.decrement');
+var genereatePassword = function (options) {
+    var digits = '0123456789';
+    var capitals = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var symbols = '!@#$%^&*()_+[]{}|;:,.<>?';
+    var password = '';
+    var base = 'abcdefghijklmnopqrstuvwxyz';
+    if (options.digits > 0)
+        base += digits;
+    if (options.capitals > 0)
+        base += capitals;
+    if (options.symbols > 0)
+        base += symbols;
+    var length = 6;
+    for (var i = 0; i < length; i++) {
+        password += base[Math.floor(Math.random() * base.length)];
+    }
+    return password;
+};
